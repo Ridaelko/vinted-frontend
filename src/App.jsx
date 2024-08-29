673ea2bbbd2036f7d66993dd514251a1f2b4a945
Ridaelko <rida.elkouch@gmail.com>
2024-08-29 10:29:25 +0200

    add route home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,23 @@
 import "./App.css";
 
-// IMPORT AXIOS
-import axios from "axios";
-
-// IMPORT USESTATE ET USEEFFECT
-import { useState, useEffect } from "react";
-
 // IMPORT DE REACT ROUTER, ET JE RENOMME BROWSERROUTER EN ROUTER
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // IMPORT DE PAGES
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 
 // IMPORT DES COMPONENTS
 import Header from "./components/Header";
 
 const App = () => {
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://lereacteur-vinted-api.herokuapp.com/v2/offers"
-        );
-        setData(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  return loading ? (
-    <p>Loading ...</p>
-  ) : (
+  return (
     <>
-      <Header data={data} />
-      <main>
-        <div className="container">
-          <article>
-            {data.offers.map((offer) => {
-              return (
-                <div className="offer">
-                  <div className="owner">
-                    <img src={offer.owner.account.avatar.url} alt="" />
-                    <p>{offer.owner.account.username} </p>
-                  </div>
-                  <img
-                    className="picture-article"
-                    src={offer.product_image.url}
-                    alt=""
-                  />
-                  <p className="price">{offer.product_price}€ </p>
-                  {offer.product_details.map((details) => {
-                    return (
-                      <div className="details">
-                        <p className="marque">{details.MARQUE}</p>{" "}
-                        <p>{details.TAILLE} </p>
-                      </div>
-                    );
-                  })}
-                </div>
-              );
-            })}
-          </article>
-        </div>
-      </main>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,64 @@
-const Home = () => {};
+// // IMPORT AXIOS
+import axios from "axios";
+
+// // IMPORT USESTATE ET USEEFFECT
+import { useState, useEffect } from "react";
+
+const Home = () => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/v2/offers"
+        );
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return loading ? (
+    <p>Loading ...</p>
+  ) : (
+    <>
+      <main>
+        <div className="container">
+          <article>
+            {data.offers.map((offer) => {
+              return (
+                <div className="offer">
+                  <div className="owner">
+                    <img src={offer.owner.account.avatar.url} alt="" />
+                    <p>{offer.owner.account.username} </p>
+                  </div>
+                  <img
+                    className="picture-article"
+                    src={offer.product_image.url}
+                    alt=""
+                  />
+                  <p className="price">{offer.product_price}€ </p>
+                  {offer.product_details.map((details) => {
+                    return (
+                      <div className="details">
+                        <p className="marque">{details.MARQUE}</p>{" "}
+                        <p>{details.TAILLE} </p>
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </article>
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default Home;
