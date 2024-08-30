f2efe352b496c08ce98b81a056e7fbfa8e1953df
Ridaelko <rida.elkouch@gmail.com>
2024-08-30 10:38:46 +0200

    add link to login

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -12,7 +12,8 @@ const Signup = () => {
           const response = axios.get(
             "https://lereacteur-vinted-api.herokuapp.com/user/signup"
           );
-          Cookies.set("token", token);
+          setData(response.data);
+          Cookies.set("token", response.data.token);
         } catch (error) {
           console.log(error);
         }
@@ -25,7 +26,7 @@ const Signup = () => {
       <div className="sign-h1">
         <h1>S'inscrire</h1>
       </div>
-      <form /* onSubmit={handleSubmit} */>
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Nom d'utilisateur" />
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Mot de passe" />
@@ -38,8 +39,9 @@ const Signup = () => {
           Conditions et Politique de Confidentialité de Vinted. Je confirme
           avoir au moins 18 ans.
         </p>
-        <button>S'inscrire</button>
+        <button type="submit">S'inscrire</button>
       </form>
+      {/* <Link to="/login" >Tu as déjà un compte ? Connecte-toi !</Link> */}
     </div>
   );
 };
